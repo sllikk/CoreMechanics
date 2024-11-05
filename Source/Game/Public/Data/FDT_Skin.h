4f4925c7019f85829e504bba464ecb63a3e3f3f8
$L1ick <145702260+sllikk@users.noreply.github.com>
2024-11-05 20:04:04 +0200

    Base

---
+++ Source/Game/Public/Data/FDT_Skin.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "FDT_Skin.generated.h"
+
+USTRUCT(BlueprintType)
+struct FDT_Skin : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	FDT_Skin() : ID_Material(0) { } 
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ID")
+	int32 ID_Material;	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="DESCRIPTION")
+	FString Material_Description;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="MATERIAL")
+	TSoftObjectPtr<UMaterialInterface> MaterialSoftObjectPtr;
+	
+};
